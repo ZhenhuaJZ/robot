ae43bc534ef923b9057258c2a39227fe5374540e
zhenhua zhang <zhenhua.zhang@student.uts.edu.au>
2020-07-31 16:28:59 +1000

    1. Integrated stanford robot to gazebo 2. changed parameter to stable gazebo simulation

--- src/stanford_quadruped/src/run_robot.py
+++ src/stanford_quadruped/src/run_robot.py
@@ -1,25 +1,33 @@
+#!/usr/bin/env python3
 import numpy as np
 import time
-from src.IMU import IMU
+import rospy
+# from src.IMU import IMU
 from src.Controller import Controller
-from src.JoystickInterface import JoystickInterface
+# from src.JoystickInterface import JoystickInterface
+from src.GazeboInterface import JointController
 from src.State import State
-from pupper.HardwareInterface import HardwareInterface
+# from pupper.HardwareInterface import HardwareInterface
+from src.Command import Command
 from pupper.Config import Configuration
 from pupper.Kinematics import four_legs_inverse_kinematics
+
 
 def main(use_imu=False):
     """Main program
     """
-
+    rospy.init_node("test_pub", anonymous=True)
+    interface = JointController()
+    interface.init_joint_publishers()
+    rate = rospy.Rate(5)
     # Create config
     config = Configuration()
-    hardware_interface = HardwareInterface()
+    # hardware_interface = HardwareInterface()
 
     # Create imu handle
-    if use_imu:
-        imu = IMU(port="/dev/ttyACM0")
-        imu.flush_buffer()
+    # if use_imu:
+    #     imu = IMU(port="/dev/ttyACM0")
+    #     imu.flush_buffer()
 
     # Create controller and user input handles
     controller = Controller(
@@ -28,7 +36,7 @@ def main(use_imu=False):
     )
     state = State()
     print("Creating joystick listener...")
-    joystick_interface = JoystickInterface(config)
+    # joystick_interface = JoystickInterface(config)
     print("Done.")
 
     last_loop = time.time()
@@ -40,40 +48,47 @@ def main(use_imu=False):
     print("x shift: ", config.x_shift)
 
     # Wait until the activate button has been pressed
-    while True:
+    while not rospy.is_shutdown():
         print("Waiting for L1 to activate robot.")
-        while True:
-            command = joystick_interface.get_command(state)
-            joystick_interface.set_color(config.ps4_deactivated_color)
-            if command.activate_event == 1:
-                break
-            time.sleep(0.1)
+        # while True:
+        #     command = joystick_interface.get_command(state)
+        #     joystick_interface.set_color(config.ps4_deactivated_color)
+        #     if command.activate_event == 1:
+        #         break
+        #     time.sleep(0.1)
         print("Robot activated.")
-        joystick_interface.set_color(config.ps4_color)
-
-        while True:
+        # joystick_interface.set_color(config.ps4_color)
+        command = Command()
+        command.horizontal_velocity = np.array([0.1, 0])
+        command.hop_event = False
+        while not rospy.is_shutdown():
+            rate.sleep()
             now = time.time()
             if now - last_loop < config.dt:
                 continue
             last_loop = time.time()
 
             # Parse the udp joystick commands and then update the robot controller's parameters
-            command = joystick_interface.get_command(state)
-            if command.activate_event == 1:
-                print("Deactivating Robot")
-                break
+            # command = joystick_interface.get_command(state)
+            # if command.activate_event == 1:
+            #     print("Deactivating Robot")
+            #     break
 
             # Read imu data. Orientation will be None if no data was available
             quat_orientation = (
                 imu.read_orientation() if use_imu else np.array([1, 0, 0, 0])
             )
+            quat_orientation = (np.array([1, 0, 0, 0]))
             state.quat_orientation = quat_orientation
 
             # Step the controller forward by dt
             controller.run(state, command)
-
+            # controller.set_pose_to_default(state)
+            print("foot_locations: ", state.foot_locations)
+            print("joint_angles: ", state.joint_angles)
+            interface(state.joint_angles)
             # Update the pwm widths going to the servos
-            hardware_interface.set_actuator_postions(state.joint_angles)
+            # hardware_interface.set_actuator_postions(state.joint_angles)
 
 
 main()
---
+++ src/stanford_quadruped/src/src/GazeboInterface.py
@@ -0,0 +1,92 @@
+import rospy
+import numpy as np
+from std_msgs.msg import Float64
+
+
+class JointController:
+    def __init__(self):
+        self.init_joint_publishers()
+        return
+
+    def init_joint_publishers(self):
+        self.FrontRightJoint1_pub = rospy.Publisher("/FrontRightJoint1_position_controller/command",
+                                                    Float64,
+                                                    queue_size=1)
+        self.FrontRightJoint2_pub = rospy.Publisher("/FrontRightJoint2_position_controller/command",
+                                                    Float64,
+                                                    queue_size=1)
+        self.FrontRightJoint3_pub = rospy.Publisher("/FrontRightJoint3_position_controller/command",
+                                                    Float64,
+                                                    queue_size=1)
+        self.FrontLeftJoint1_pub = rospy.Publisher("/FrontLeftJoint1_position_controller/command",
+                                                   Float64,
+                                                   queue_size=1)
+        self.FrontLeftJoint2_pub = rospy.Publisher("/FrontLeftJoint2_position_controller/command",
+                                                   Float64,
+                                                   queue_size=1)
+        self.FrontLeftJoint3_pub = rospy.Publisher("/FrontLeftJoint3_position_controller/command",
+                                                   Float64,
+                                                   queue_size=1)
+        self.BackRightJoint1_pub = rospy.Publisher("/BackRightJoint1_position_controller/command",
+                                                     Float64,
+                                                     queue_size=1)
+        self.BackRightJoint2_pub = rospy.Publisher("/BackRightJoint2_position_controller/command",
+                                                     Float64,
+                                                     queue_size=1)
+        self.BackRightJoint3_pub = rospy.Publisher("/BackRightJoint3_position_controller/command",
+                                                     Float64,
+                                                     queue_size=1)
+        self.BackLeftJoint1_pub = rospy.Publisher("/BackLeftJoint1_position_controller/command",
+                                                    Float64,
+                                                    queue_size=1)
+        self.BackLeftJoint2_pub = rospy.Publisher("/BackLeftJoint2_position_controller/command",
+                                                    Float64,
+                                                    queue_size=1)
+        self.BackLeftJoint3_pub = rospy.Publisher("/BackLeftJoint3_position_controller/command",
+                                                    Float64,
+                                                    queue_size=1)
+
+    def pos_reset(self):
+        self.control_front_right_joint(0.01, 0.01, 0.01)
+        self.control_front_left_joint(0.01, 0.01, 0.01)
+        self.control_back_right_joint(0.01, 0.01, 0.01)
+        self.control_back_left_joint(0.01, 0.01, 0.01)
+
+    def control_front_right_joint(self, joint1=None, joint2=None, joint3=None):
+        if joint1:
+            self.FrontRightJoint1_pub.publish(data=joint1)
+        if joint2:
+            self.FrontRightJoint2_pub.publish(data=joint2)
+        if joint3:
+            self.FrontRightJoint3_pub.publish(data=joint3)
+
+    def control_front_left_joint(self, joint1=None, joint2=None, joint3=None):
+        if joint1:
+            self.FrontLeftJoint1_pub.publish(data=joint1)
+        if joint2:
+            self.FrontLeftJoint2_pub.publish(data=joint2)
+        if joint3:
+            self.FrontLeftJoint3_pub.publish(data=joint3)
+
+    def control_back_right_joint(self, joint1=None, joint2=None, joint3=None):
+        if joint1:
+            self.BackRightJoint1_pub.publish(data=joint1)
+        if joint2:
+            self.BackRightJoint2_pub.publish(data=joint2)
+        if joint3:
+            self.BackRightJoint3_pub.publish(data=joint3)
+
+    def control_back_left_joint(self, joint1=None, joint2=None, joint3=None):
+        if joint1:
+            self.BackLeftJoint1_pub.publish(data=joint1)
+        if joint2:
+            self.BackLeftJoint2_pub.publish(data=joint2)
+        if joint3:
+            self.BackLeftJoint3_pub.publish(data=joint3)
+
+    def __call__(self, joints):
+        joints[1, :] -= np.pi/3
+        self.control_front_right_joint(-joints[0, 0], -joints[1, 0], -joints[2, 0])
+        self.control_front_left_joint(joints[0, 1], joints[1, 1], joints[2, 1])
+        self.control_back_right_joint(-joints[0, 2], -joints[1, 2], -joints[2, 2])
+        self.control_back_left_joint(joints[0, 3], joints[1, 3], joints[2, 3])
